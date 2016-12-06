2608ad537b9a33eb82246b480be978c419ee92b5
Linus Quan <Linus@Linuss-MacBook-Pro.local>
2016-12-06 23:43:13 +1100

    'this' keyword points to different things in different context

--- pages/app.js
+++ pages/app.js
@@ -1,19 +1,55 @@
-function greet() {
-    console.log('Hi');
+// console.log(this);
+
+function a() {
+    console.log(this);
+    this.name = "Linus Quan";
 }
 
-// anoynmousF(); // cannot be used here, variable is undefined
+a(); // simply invoking function, this refers to global context
 
-var anoynmousF = function () {
-    console.log('Hello');
-};
+var b = function () {
+    console.log(this);
+}
+console.log(name); // Linus Quan gets output since this in a() refers to global context
+b(); // all global this
 
-anoynmousF();
 
-function log(a) {
-    a();
+// In object method
+// var obj = {
+//     name: "javascript name",
+//     log: function () {
+//         this.name = "updated js name";
+//         console.log(this); // this refers to obj in which property name gets changed to new value
+//         var setname = function (newname) {
+//             this.name = newname;
+//         }
+//         setname("set a new name");
+//         // a weird bug
+//         console.log(this);
+//         // try window in console and you can see the name is set in global context
+//     }
+//
+// }
+
+
+
+// A better practice:
+
+var obj = {
+    name: "javascript name",
+    log: function () {
+        var self = this; // objects set by reference, self points to the same address of this
+        self.name = "updated js name";
+        console.log(self); // this refers to obj in which property name gets changed to new value
+        var setname = function (newname) {
+            self.name = newname;
+        }
+        setname("set a new name");
+        // a weird bug
+        console.log(self);
+        // try window in console and you can see the name is set in global context
+    }
 }
 
-log(function () {
-    console.log("Hi from function as param");
-})
+
+obj.log();
