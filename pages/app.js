e58617bc08d7d90006fb4bd343d6f056b0317c3c
Linus Quan <Linus@Linuss-MacBook-Pro.local>
2016-12-06 14:27:31 +1100

    JSON vs js object

--- pages/app.js
+++ pages/app.js
@@ -1,14 +1,9 @@
-// var greet = "Hello";
-// var greet = "Hola"; // collision
+var objectLiteral = {
+        firstname: "Mary",
+        isProgrammer: true
+};
 
+console.log(JSON.stringify(objectLiteral)); // a built-in function to translate js obj to JSON
 
-var english = {greetings:{}};
-
-var spanish = {};
-
-english.greet = "Hello";
-spanish.greet = "Hola";
-
-// namespace inside namespace
-//english.greeting = {};
-english.greetings.greet = "Hello";
+var jsonValue = JSON.parse('{ "firstname": "firstname", "isProgrammer": true}');
+console.log(jsonValue);
--- pages/note.md
+++ pages/note.md
@@ -1,3 +1,20 @@
-### Fake namespace
-- javascript does not have namespace
-- we can use {} to fake namesapce
+# JSON
+- JSON is not exact the same as js object
+- JSON is inspired by js notation
+- JSON is subset of js obj notation
+- anything valid in JSON is js but not the other way around
+### JSON vs js
+```javascript
+// fine in only js
+var objectLiteral = 
+{ 
+    firstname: "Mary",
+    isProgrammer: true
+}
+// fine in JSON AND JS
+{
+  "firstname": "firstname",
+  "isProgrammer": true
+}
+
+```
